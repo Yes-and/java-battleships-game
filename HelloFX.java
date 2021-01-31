cb7d3ef210efb640dce22c6d427df6a70018586b
Adam Bobocky <adambobocky@gmail.com>
2021-01-31 19:43:10 +0100

    Update1

--- HelloFX.java
+++ HelloFX.java
@@ -5,12 +5,9 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
-
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Label l = new Label("Welcome to a really bad battleships game!");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
@@ -19,5 +16,4 @@ public class HelloFX extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
--- Runner.java
+++
@@ -1,7 +0,0 @@
-import CSA.*;
-
-public class Runner {
-    public static void main(String[] args) {
-        
-    }
-}
