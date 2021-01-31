3c1387aa53592c1c356aaa71d438d9aebfc04253
Adam Bobocky <adambobocky@gmail.com>
2021-01-31 20:32:37 +0100

    Pizza

--- HelloFX.java
+++ HelloFX.java
@@ -1,19 +1,33 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class HelloFX extends Application {
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
+        TilePane root = new TilePane();
+        Button startGameBtn = new Button("Start new game");
+        Font font = Font.font("comic sans ms", FontWeight.BOLD, 36);
+        startGameBtn.setFont(font);
+        startGameBtn.setOnAction(actionEvent ->  {
+          System.out.println("Dusan don't read this!");
+        });
         Label l = new Label("Welcome to a really bad battleships game!");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        l.setFont(font);
+        Scene scene = new Scene(root, 300, 300);
+        root.getChildren().add(l);
+        root.getChildren().add(startGameBtn);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Button Class Example");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
