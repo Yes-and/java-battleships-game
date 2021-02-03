280da4505fd2be3059a3fa41e68baa40e19f1b7a
Adam Bobocky <adambobocky@gmail.com>
2021-02-03 22:46:29 +0100

    Add grids UI

--- GameFX.java
+++ GameFX.java
@@ -2,7 +2,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -11,23 +13,59 @@ import CSA.Battlefield;
 public class GameFX {
   private Battlefield PlayerField = new Battlefield();
   private Battlefield EnenyField = new Battlefield();
-  private final TilePane rootPane;
+  private final GridPane rootPane;
 
   public GameFX(Battlefield playerField, Stage primaryStage, TilePane menuRoot) {
     this.PlayerField = playerField;
 
-    rootPane = new TilePane();
+    rootPane = new GridPane();
 
-    Font font = Font.font("comic sans ms", FontWeight.BOLD, 36);
+    Font font = Font.font("comic sans ms", FontWeight.BOLD, 18);
 
     Button mainMenuBtn = new Button("Quit to main menu");
     mainMenuBtn.setFont(font);
     mainMenuBtn.setOnAction(actionEvent -> {
       primaryStage.getScene().setRoot(menuRoot);
     });
-    rootPane.getChildren().add(mainMenuBtn);
+    rootPane.add(mainMenuBtn, 1, 0);
+
+    Label playerGridL = new Label("Player grid:");
+    playerGridL.setFont(font);
+    rootPane.add(playerGridL, 0, 1);
+
+    GridPane playerGridPane = new GridPane();
+
+    for(int x = 0; x < 8; x++){
+      for(int y = 0; y < 8; y++){
+        Button shipOneBtn = new Button(x + " " + y);
+        shipOneBtn.setFont(font);
+        shipOneBtn.setOnAction(actionEvent -> {
+        });
+        playerGridPane.add(shipOneBtn, x, y);
+      }
+    }
+
+    rootPane.add(playerGridPane, 0, 2);
+
+    Label enemyGridL = new Label("Enemy grid:");
+    enemyGridL.setFont(font);
+    rootPane.add(enemyGridL, 3, 1);
+
+    GridPane enemyGridPane = new GridPane();
+
+    for(int x = 0; x < 8; x++){
+      for(int y = 0; y < 8; y++){
+        Button shipOneBtn = new Button(x + " " + y);
+        shipOneBtn.setFont(font);
+        shipOneBtn.setOnAction(actionEvent -> {
+        });
+        enemyGridPane.add(shipOneBtn, x, y);
+      }
+    }
+
+    rootPane.add(enemyGridPane, 3, 2);
   }
-  public TilePane getRootPane() {
+  public GridPane getRootPane() {
     return rootPane;
   }
 }
--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,3 +1,4 @@
+rm *.class
 javac --module-path $PATH_TO_FX --add-modules javafx.controls MenuFX.java
 javac --module-path $PATH_TO_FX --add-modules javafx.controls,javafx.fxml MenuFX.java
 java --module-path $PATH_TO_FX --add-modules javafx.controls MenuFX
