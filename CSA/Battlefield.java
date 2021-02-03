9cb0a499c6c103db2c7b64ab5d2d77693c1aadfd
git <vojtawilson@gmail.com>
2021-02-03 08:28:01 +0100

    More ships

--- CSA/Battlefield.java
+++ CSA/Battlefield.java
@@ -21,6 +21,22 @@ public class Battlefield {
 
     }
 
+    // Get player's battlefield
+    public int[][] GetOwnShip() {
+        return this.PopulatedBattlefield;
+    }
+
+    // Get the confirmed hits/misses
+    public int[][] GetSpotShip() {
+        return this.SpottedBattlefield;
+    }
+
+    // Ship setter for player's battlefield
+    public void SetOwnShip(int [][] grid) {
+        this.PopulatedBattlefield = grid;
+    }
+
+    // Create a new ship
     public int[][] MakeShip(int[][] grid, int length, int ID) {
 
         Boolean isHorizontal = Math.random() > 0.5;
@@ -40,5 +56,15 @@ public class Battlefield {
         return grid;
 
     }
+
+    // Show own battlefield in console
+    public void toStringOwnB() {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                System.out.println(this.PopulatedBattlefield;[x][y])
+            }
+        }
+    }
+
 
 }
